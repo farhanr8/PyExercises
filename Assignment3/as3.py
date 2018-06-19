c1946c26b0c38dc460894f1539e5966dc61ff3e3
rahman <rahman@arlut.utexas.edu>
2018-06-19 15:42:48 -0500

    Game updates: Needs fixes

--- Assignment3/as3.py
+++ Assignment3/as3.py
@@ -30,11 +30,76 @@ class ZombieCharacter(ICharacter):
 
         return MoveEvent(self, x + x_off, y + y_off)
 
+
 class PlayerCharacter(ICharacter):
     def __init__(self, obj_id, health, x, y, map_view):
         ICharacter.__init__(self, obj_id, health, x, y, map_view)
         # You may add any instance attributes you find useful to save information between frames
+        self.game_frame = 0
+        self.closest_zombie_x = map_view.getMapSize()[0]
+        self.closest_zombie_y = map_view.getMapSize()[1]
+        self.target_zombie = 0
 
     def selectBehavior(self):
-        # Replace the body of this method with your character's behavior
-        pass
+
+        self.game_frame += 1
+
+        scan_results = self.getScanResults()
+
+        x_off, y_off = 1, 1
+        map_view = self.getMapView()
+        size_x, size_y = map_view.getMapSize()
+        x, y = self.getPos()
+
+        # HEAL
+        prob = random.random()
+
+        # If health is less than 40%, then heal with a 30% probability
+        if prob < 0.3 and self.getHealth() < self.getInitHealth() * 0.4:
+            return HealEvent(self)
+
+        # SCAN
+        if self.game_frame == 1:
+            return ScanEvent(self)
+        if self.game_frame != 2 and self.game_frame % 2 == 0:
+            return ScanEvent(self)
+
+        # If no zombies nearby, don't do anything. TODO: Make it efficient
+        if len(scan_results) == 0:
+            return MoveEvent(self, x, y)
+
+        for res in scan_results:
+            pos = res.getPos()
+
+            if pos[0] <= self.closest_zombie_x and pos[1] <= self.closest_zombie_y:
+                self.closest_zombie_x = pos[0]
+                self.closest_zombie_y = pos[1]
+                self.target_zombie = res.getID()
+
+        # ATTACK
+        if 3 > abs(x - self.closest_zombie_x) > 0:
+            if 3 > abs(y - self.closest_zombie_y) > 0:
+                return AttackEvent(self, self.target_zombie)
+
+        # MOVE
+
+        # Run away. Too close to zombies    TODO: Fix this
+        if abs(x - self.closest_zombie_x) < 1:
+            if self.closest_zombie_x > x:
+                x_off = -1
+        if abs(y - self.closest_zombie_y) < 1:
+            if self.closest_zombie_y > y:
+                y_off = -1
+
+        # At the edge
+        if x + x_off < 0 or x + x_off >= size_x:
+            x_off = 0
+        if y + y_off < 0 or y + y_off >= size_y:
+            y_off = 0
+
+        return MoveEvent(self, x + x_off, y + y_off)
+
+
+
+
+
--- testing.py
+++ testing.py
@@ -1,44 +1,11 @@
-def test(str):
-    str = str.split(" ")
-    nums = []
-    for c in str:
-        nums.append(c[0])
-
-    for i in range(4, -1, -1):
-        print(i)
-
-    nums.sort()
-    while 'T' in nums:
-        ind = nums.index('T')
-        nums[ind] = 'B'
-        nums.sort()
-
-    while 'A' in nums:
-        ind = nums.index('A')
-        nums[ind] = 'Z'
-        nums.sort()
-
-    while 'K' in nums:
-        ind = nums.index('K')
-        nums[ind] = 'Y'
-        nums.sort()
-
-    while 'B' in nums:
-        ind = nums.index('B')
-        nums[ind] = 'T'
-
-    while 'Y' in nums:
-        ind = nums.index('Y')
-        nums[ind] = 'K'
-
-    while 'Z' in nums:
-        ind = nums.index('Z')
-        nums[ind] = 'A'
-
-    return nums
-
+import random
 
 def main():
-    print(test("KC TH JS TH QD"))
 
-if __name__ == '__main__': main()
+
+    for i in range(5):
+        prob = random.random()
+        print(prob)
+
+if __name__ == '__main__':
+    main()
